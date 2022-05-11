6122f435422dc7a06edd3c6e41d4ad06c6be5937
David Petryca <david@petryca.com>
2022-05-11 11:28:13 +0200

    add colors

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,21 @@
 <script>
-import { onMount } from 'svelte';
+import { onMount, tick } from 'svelte';
 import Sticky from './Sticky.svelte';
 import Button from './Button.svelte';
 
-let stickies = [{
-	s: ''
-}];
+const options = {  year: 'numeric', month: 'numeric', day: 'numeric'};
+let now = new Date().toLocaleDateString(undefined, options);
+
+let stickies = [];
+
+window.onbeforeunload = () => {
+	for (let i = 0; i < stickies.length; i++) { 
+		if (stickies[i].s === '') {
+			stickies.splice(i, 1);
+		}
+	}
+	saveStickies();
+}
 
 onMount(async () => {
 	if (window.localStorage.getItem('stickies')) {
@@ -14,46 +24,50 @@ onMount(async () => {
 			document.querySelector('textarea:first-of-type').focus();
 		}
 	}
-	window.onbeforeunload = () => {
-		for (let i = 0; i < stickies.length; i++) { 
-			if (stickies[i].s === '') {
-				stickies.splice(i, 1);
-			}
-		}
-		saveStickies();
-	}
 });
 
 function  saveStickies() {
-	console.log(stickies);
 	const json = JSON.stringify(stickies);
 	window.localStorage.setItem('stickies', json);
 }
 
-function deleteSticky(e) {
-	if (stickies.length > 1) {
-		document.querySelector( ':focus' ).blur();
-		stickies.splice(e.detail, 1);
-		stickies = stickies;
-		saveStickies();
+async function deleteSticky(e) {
+	const previousEl = document.activeElement.previousElementSibling;
+	stickies.splice(e.detail, 1);
+	stickies = stickies;
+	saveStickies();
+	await tick();
+	if (stickies.length && previousEl.nodeName == 'TEXTAREA') {	
+		previousEl.focus();
+		previousEl.setSelectionRange(previousEl.value.length, previousEl.value.length);
 	}
 }
 
-function unshiftSticky() {
-	stickies.unshift({s: ''});
+async function unshiftSticky() {
+	stickies.unshift({s: now + ' ', c: 0});
 	stickies = stickies;
-	document.querySelector('textarea:first-of-type').focus();
+	await tick();
+	let element = document.querySelector('textarea:first-of-type');
+	element.focus();
+	element.setSelectionRange(element.value.length, element.value.length);
 }
 
-function keyControl() {
-	const firstEl = document.querySelector('textarea:first-of-type');
+async function tabControl() {
     const lastEl = document.querySelector('textarea:last-of-type');
 	const focusedEl = document.activeElement;
 	
-	if (focusedEl !== lastEl) {
-		focusedEl.nextElementSibling.focus();
+	if (focusedEl === lastEl) {
+		stickies.push({s: now + ' ', c: 0});
+		stickies = stickies;
+		await tick();
+		let element = document.querySelector('textarea:last-of-type');
+		element.focus();
+		element.setSelectionRange(element.value.length, element.value.length);
+		console.log(element);
 	} else {
-		firstEl.focus();
+		let element = focusedEl.nextElementSibling;
+		element.focus();
+		element.setSelectionRange(element.value.length, element.value.length);
 	}
 }
 
@@ -61,7 +75,7 @@ function keyControl() {
 
 <main>
 	<Button on:click={unshiftSticky} />
-	{#each stickies as sticky, i}
-		<Sticky data={sticky} index={i} on:keyenter={keyControl} on:delete={deleteSticky} on:blur={saveStickies} />
+	{#each stickies as data, i}
+		<Sticky bind:data index={i} on:tab={tabControl} on:delete={deleteSticky} on:blur={saveStickies} />
 	{/each}
 </main>
--- src/Sticky.svelte
+++ src/Sticky.svelte
@@ -2,25 +2,37 @@
     export let data;
     export let index;
     import { createEventDispatcher } from 'svelte';
-    
+
     const dispatch = createEventDispatcher();
+    const colors = ['#6eed2a', '#fcfa5d', '#f989d6', '#20dff8'];
+
+    function updateColor(e) {
+        data.c = (data.c + 1) % colors.length;
+        e.target.style.backgroundColor = [data.c];
+    }
 
     function keyPressed(e) {
-        if ( e.key === 'Enter' || e.key === 'Tab') {
+        if (e.key === 'Enter') {
             e.preventDefault();
-            dispatch('keyenter');
+            updateColor(e);
+        } else if (e.key === 'Tab') {
+            e.preventDefault();
+            dispatch('tab');
         } else if (e.key === 'Backspace' && data.s === '') {
+            e.preventDefault();
             dispatch('delete', index);
         }
     }
 </script>
 
-<textarea maxlength="128" on:keydown={keyPressed} on:blur bind:value={data.s}></textarea>
+<textarea style="background-color:{colors[data.c]};" maxlength="128" on:keydown={keyPressed} on:blur bind:value={data.s}></textarea>
 
 <style>
     textarea {
         display: block;
-        background-color: #fff6e1;
+        box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+        position: relative; 
+        background-color: #6eed2a;
         padding: 1rem;
         flex-basis: 20%;
         transition: all 0.3s;
@@ -32,6 +44,6 @@
         line-height: inherit;
     }
     textarea:focus {
-        background-color: #fffcf7;
+        box-shadow: 0 6px 12px rgba(0, 0, 0, 0.4);
     }
 </style>
